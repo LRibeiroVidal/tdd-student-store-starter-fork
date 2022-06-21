9bbff29ec536c4eaecd15a5c15fbfa14551aacf5
Lucas Ribeiro Vidal <lribeirovidal@salesforce.com>
2022-06-21 16:48:06 -0700

    started work on the sidebar

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,13 @@
 import * as React from "react"
 import "./Sidebar.css"
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 
-export default function Sidebar() {
-  return (
-    <section className="sidebar">
-      <p>Sidebar</p>
+export default function Sidebar({isOpen = false, shoppingCart, products, checkoutForm, handleOnCheckoutFormChange, handleOnSubmitCheckoutForm, handleOnToggle = () => {}}) {
+  return ( 
+    <section className={isOpen ? "sidebar" : "sidebar closed"}>
+      <button className="toggle-button" onClick={handleOnToggle}>
+        <ArrowRightIcon style={{color: "black",   backgroundColor: "white"}}/>
+      </button>
     </section>
   )
 }
