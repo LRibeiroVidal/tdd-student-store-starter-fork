8a79368c9e22888615e69df3210139e2df7fc3bb
Lucas Ribeiro Vidal <lribeirovidal@salesforce.com>
2022-06-29 13:43:30 -0700

    added local api and /store endpoint

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,1 +1,22 @@
 // YOUR CODE HERE
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const { storage } = require("./data/storage.js");
+
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+	res.status(200).send({ ping: "pong" });
+});
+
+app.get("/store", (req, res) => {
+	res.status(200).send({ products: storage.get("products").value() });
+	console.log({ data: storage.get("products").value() });
+});
+
+module.exports = app;
